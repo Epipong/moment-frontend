70af60a3deaabbc69588f0afca2e3ddf0a77bdfc
davy.tran <davytran1993@gmail.com>
2024-07-14 17:26:13 +0200

    Add Register form

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <></>
+  );
+};
